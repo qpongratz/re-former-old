574a9e2bbed97e4fdf4c647a98ce268f132f14b0
qpongratz <lagarathan@gmail.com>
2021-09-16 11:17:17 -0500

    Create now adds to database on successful submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_users_path
+    else
+      render :new
+    end
   end
 end
